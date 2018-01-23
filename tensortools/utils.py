2df408e16a94dd17d4b27cc7c173add5f1d87530
Alex Williams <alex.h.willia@gmail.com>
2018-01-22 20:42:09 -0800

    add travelling salesperson sorting

--- tensortools/utils.py
+++ tensortools/utils.py
@@ -3,6 +3,7 @@ Useful helper functions, not critical to core functionality of tensortools.
 """
 
 import numpy as np
+import scipy
 import math
 
 def coarse_grain_1d(tensor, factor, axis=0, reducer=np.sum,
@@ -83,3 +84,112 @@ def soft_cluster_factor(factor):
         perm += list(idx[np.argsort(scores[idx])][::-1])
 
     return cluster_ids, perm
+
+def resort_factor_tsp(factor, niter=1000, metric='euclidean', **kwargs):
+    """Sorts the factor to (approximately) to solve the traveling
+    salesperson problem, so that data elements (rows of factor)
+    are placed closed to each other.
+    """
+
+    # Compute pairwise distances between all datapoints
+    N = factor.shape[0]
+    D = scipy.spatial.distance.pdist(factor, metric=metric, **kwargs)
+    
+    # To solve the travelling salesperson problem with no return to the original node
+    # we add a dummy node that has distance zero connections to all other nodes. The
+    # dummy node is then removed after we've converged to a solution
+    dist = np.zeros((N+1, N+1))
+    dist[:N,:N] = scipy.spatial.distance.squareform(D)
+    
+    # solve TSP
+    path, cost_hist = solve_tsp(D)
+    
+    # remove dummy node at position i
+    i = np.argwhere(path==N).ravel()[0]
+    path = np.hstack((path[(i+1):], path[:i]))
+    
+    return path, cost_hist
+
+
+def reverse_segment(path, n1, n2):
+    """Reverse the nodes between n1 and n2.
+    """
+    q = path.copy()
+    if n2 > n1:
+        q[n1:(n2+1)] = path[n1:(n2+1)][::-1]
+        return q
+    else:
+        seg = np.hstack((path[n1:], path[:(n2+1)]))[::-1]
+        brk = len(q) - n1
+        q[n1:] = seg[:brk]
+        q[:(n2+1)] = seg[brk:]
+        return q
+
+def solve_tsp(dist):
+    """Solve travelling salesperson problem (TSP) by two-opt swapping.
+    
+    Params
+    ------
+    dist (ndarray) : distance matrix
+    
+    Returns
+    -------
+    path (ndarray) : permutation of nodes in graph (rows of dist matrix)
+    """
+
+    # number of nodes
+    N = dist.shape[0]
+
+    # tsp path for quick calculation of cost
+    ii = np.arange(N)
+    jj = np.hstack((np.arange(1, N), 0))
+
+    # for each node, a sorted list of closest nodes
+    dsort = [np.argsort(d) for d in dist]
+    dsort = [d[d != i] for i, d in enumerate(dsort)]
+
+    # randomly initialize path through graph
+    path = np.random.permutation(N)
+    idx = np.argsort(path)
+    cost = np.sum(dist[path[ii], path[jj]])
+    
+    # keep track of objective function over time
+    cost_hist = [cost]
+
+    # optimization loop
+    node = 0
+    while node < N:
+
+        # we'll try breaking the connection i -> j
+        i = path[node]
+        j = path[(node+1) % N]
+        
+        # since we are breaking i -> j we can remove the cost of that connection
+        c = cost - dist[i, j]
+
+        # search over nodes k that are closer to j than i
+        for k in dsort[j]:
+            # can safely continue if dist[i,j] < dist[k,j] for the remaining k
+            if k == i:
+                node += 1
+                break
+
+            # break connection k -> p
+            # add connection j -> p
+            # add connection i -> k
+            p = path[(idx[k]+1) % N]
+            new_cost = c - dist[k,p] + dist[j,p] + dist[i,k]
+
+            # if this swap improves the cost, implement it and move to next i
+            if new_cost < cost:
+                path = reverse_segment(path, idx[j], idx[k])
+                idx = np.argsort(path)
+                # make sure that we didn't screw up
+                assert np.abs(np.sum(dist[path[ii], path[jj]]) - new_cost) < 1e-6
+                cost = new_cost
+                # restart from the begining of the graph
+                cost_hist.append(cost)
+                node = 0
+                break
+
+    return path, cost_hist
