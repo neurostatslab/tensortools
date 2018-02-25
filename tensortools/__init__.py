136a167f1dde735dad6c8501cccd8ba4d1ec17e1
Ben Erichson <erichson@uw.edu>
2018-02-25 13:42:07 -0800

    update als function

--- tensortools/__init__.py
+++ tensortools/__init__.py
@@ -5,3 +5,5 @@ Tensortools
 from .ensemble import Ensemble
 from .tensors import Ktensor
 from .diagnostics import *
+from .data.random_tensor import random_tensor
+from .optimize.cp_als import cp_als
