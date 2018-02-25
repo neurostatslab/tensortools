588b2292a74afd584189d606a0b31e64f8e29308
Ben Erichson <erichson@uw.edu>
2018-02-25 13:42:42 -0800

    test suite;

---
+++ tests/tests.py
@@ -0,0 +1,90 @@
+from __future__ import division
+import numpy as np
+import scipy as sci
+
+
+from tensortools.optimize import cp_als
+from tensortools.operations import khatri_rao
+from tensortools.tensors import Ktensor
+from tensortools.data import random_tensor
+
+
+from unittest import main, makeSuite, TestCase, TestSuite
+from numpy.testing import assert_raises, assert_equal
+
+atol_float32 = 1e-4
+atol_float64 = 1e-8
+
+random_state = 123
+
+#
+#******************************************************************************
+#
+class test_base(TestCase):
+    def setUp(self):
+        np.random.seed(123)
+
+    def test_khatrirao(self):
+        A = np.array([ 
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+        ])
+	    
+        B = np.array([
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9]
+        ])
+
+        C = np.array([
+        [1, 8, 21],
+        [2, 10, 24],
+        [3, 12, 27],
+        [4, 20, 42],
+        [8, 25, 48],
+        [12, 30, 54],
+        [7, 32, 63],
+        [14, 40, 72],
+        [21, 48, 81]
+        ])
+
+        assert np.allclose(khatri_rao((A, B)), C, atol_float64)   
+
+
+
+     
+
+#
+#******************************************************************************
+#
+class test_cp_als(TestCase):
+    def setUp(self):
+        np.random.seed(123)        
+        
+    def test_cp_als_deterministic(self):
+        I,J,K,R = 15,15,15,3
+        X = random_tensor((I,J,K), rank=R, full=True, random_state=random_state)         
+        P = cp_als(X, rank=R, trace=False)  
+                
+        percent_error = sci.linalg.norm(P.U.full() - X) / sci.linalg.norm(X)
+        assert percent_error < atol_float32   
+
+
+
+
+#
+#******************************************************************************
+#
+        
+def suite():
+    s = TestSuite()
+    s.addTest(test_base('test_khatrirao'))
+    s.addTest(test_cp_als('test_cp_als_deterministic'))
+
+
+    
+    return s
+
+if __name__ == '__main__':
+    main(defaultTest = 'suite')
