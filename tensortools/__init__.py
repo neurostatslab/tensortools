76b464754e8a2e33fc3004fc5c4927ea038be8eb
Ben Erichson <erichson@uw.edu>
2018-02-27 18:20:32 -0800

    uniform distributed CP tensor

--- tensortools/__init__.py
+++ tensortools/__init__.py
@@ -5,6 +5,6 @@ Tensortools
 from .ensemble import Ensemble
 from .tensors import Ktensor
 from .diagnostics import *
-from .data.random_tensor import randn_tensor
+from .data.random_tensor import randn_tensor, rand_tensor
 from .optimize.cp_als import cp_als
 from .optimize.ncp_hals import ncp_hals
