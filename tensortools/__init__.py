17850ffe12e834fb805f9f63ebbecaa04dc26a04
ahwillia <alex.h.willia@gmail.com>
2018-08-02 23:35:17 -0700

    fix merge artifact

--- tensortools/__init__.py
+++ tensortools/__init__.py
@@ -9,17 +9,9 @@ from .diagnostics import kruskal_align
 
 from .visualization import plot_factors, plot_objective, plot_similarity
 
-<<<<<<< HEAD
 from .data.random_tensor import randn_tensor, rand_tensor
-=======
-# useful, non-critical functions
-from .tensor_utils import unfold
-from . import utils
->>>>>>> alex
 
 from .optimize.cp_als import cp_als
 from .optimize.cp_opt import cp_opt
 from .optimize.ncp_hals import ncp_hals
 from .optimize.ncp_bcd import ncp_bcd
-
-from .utils import soft_cluster, tsp_linearize
